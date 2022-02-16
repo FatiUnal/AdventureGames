84e578bd498fb539b427573faecc09cadacf9e7c
Fatih Ünal <200401011@ogr.comu.edu.tr>
2022-02-17 00:25:31 +0300

    oyun başlıyor eklendi

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Oyun başlıyor");
 
         Game game = new Game();//burda kalabalık yapmasın diye game clasının
         game.login(); // içine tasıdık ve oyunucunun ismini aldık
